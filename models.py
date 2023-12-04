aa051816aded897cec5324cab801d7003078a5bc
Conrad Kambi <kambiconrad@gmail.com>
2023-12-04 22:59:13 +0300

    Practice completed

--- models.py
+++ models.py
@@ -1,11 +1,17 @@
 from datetime import datetime
+
+from sqlalchemy import create_engine, desc
 from sqlalchemy import Column, DateTime, Integer, String
+
 from sqlalchemy.ext.declarative import declarative_base
+
+engine = create_engine("sqlite:///migrations_test.db")
 
 Base = declarative_base()
 
+
 class Student(Base):
-    __tablename__ = 'students'
+    __tablename__ = "students"
 
     id = Column(Integer(), primary_key=True)
     name = Column(String(), index=True)
@@ -15,4 +21,4 @@ class Student(Base):
     enrolled_date = Column(DateTime(), default=datetime.now())
 
     def __repr__(self):
-        return f"Student {self.id}: {self.name}, Grade {self.grade}"
+        return f"Student {self.id}: " + f"{self.name}, " + f"Grade {self.grade}"
